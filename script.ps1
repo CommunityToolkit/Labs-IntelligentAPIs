6fb41db3bda67119ac67519ebd7b3f6fab868349
Amrutha Srinivasan <amsrin@microsoft.com>
2021-06-22 18:52:13 -0700

    updated powershell to include yolo

--- script.ps1
+++ script.ps1
@@ -1,5 +1,5 @@
 Set-Location ./models
-git lfs pull --include="vision/classification/squeezenet/model/squeezenet1.1-7.onnx", "vision/object_detection_segmentation/yolov4/model/yolov4.onnx" --exclude=""
+git lfs pull --include=["vision/classification/squeezenet/model/squeezenet1.1-7.onnx", "vision/object_detection_segmentation/yolov4/model/yolov4.onnx"] --exclude=""
 
 Set-Location ..
 Copy-Item "./models/vision/classification/squeezenet/model/squeezenet1.1-7.onnx" "./ProjectBangalore/Assets"
@@ -8,4 +8,4 @@ Copy-Item "./models/vision/object_detection_segmentation/yolov4/model/yolov4.onn
 
 Rename-Item "./ProjectBangalore/Assets/squeezenet1.1-7.onnx" "model.onnx"
 
-Rename-Item "./ProjectYOLO/Assets/yolov4.onnx" "Yolo.onnx"
+Rename-Item "./ProjectYOLO/Assets/yolov4.onnx" "yolo.onnx"
