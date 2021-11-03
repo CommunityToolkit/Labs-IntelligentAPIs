c53a4a81e0428a2bc1e640e533090930c1f7c15d
amrutha95 <amrutha.srinivasan95@gmail.com>
2021-11-03 14:33:35 -0700

    Update script.ps1

--- IntelligentAPI_EmotionRecognizer/Scripts/script.ps1
+++ IntelligentAPI_EmotionRecognizer/Scripts/script.ps1
@@ -1,7 +1,9 @@
 $ProgressPreference = 'SilentlyContinue'
 
 $emotionferplusfile = "./Assets/model_emotion.onnx"
+Write-Host "Outside if"
 if (-not(Test-Path -Path $emotionferplusfile -PathType Leaf)) {
+	Write-Host "Inside if"
      try {
          Invoke-WebRequest -URI "https://github.com/onnx/models/raw/master/vision/body_analysis/emotion_ferplus/model/emotion-ferplus-8.onnx" -OutFile $emotionferplusfile
          Write-Host "The file [$emotionferplusfile] has been created."
